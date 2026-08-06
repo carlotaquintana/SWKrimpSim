bbda9182647051d9c270248f4663a4ad389a3994
carlotaquintana <karlotakintana@gmail.com>
2026-08-06 11:21:46 +0200

    minor fix

--- SWPattern/src/com/irisa/fptree/Main.java
+++ SWPattern/src/com/irisa/fptree/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Main {
 
-    static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         if (args.length != 3) {
 
